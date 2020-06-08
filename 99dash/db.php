2940294c5e3f487d6d3fbc07e8f2dfbaa9ff89af
Bruno Brito <xbrunots@gmail.com>
2020-06-08 11:07:30 -0300

    cleaning

--- 99dash/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$username = 'u915481333_torre';
-$password = 'ana010118';
-$connection = new PDO( 'mysql:host=sql157.main-hosting.eu;dbname=u915481333_torre', $username, $password );
-
-?> 
